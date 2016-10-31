87c2a5a8b54a9b490d006f642428d8f54f3942af
Jason McCallister <themccallister@gmail.com>
2016-10-31 16:06:52 -0400

    Adding Redis support for sessions

--- public/index.php
+++ public/index.php
@@ -6,6 +6,20 @@ $craftPath = '../';
 // Do not edit below this line
 $path = rtrim($craftPath, '/').'/app/index.php';
 
+// attempt to load PHPDotenv from composer
+// otherwise assume and set production env
+try {
+    $dotenv = new Dotenv\Dotenv(dirname(__DIR__));
+    $dotenv->load();
+    $dotenv->required([
+        'APP_CPTRIGGER', 'APP_NAME', 'APP_KEY', 
+        'DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_PREFIX'
+    ]);
+} catch(\Exception $e) {
+    // assume production
+    putenv('APP_ENV=production');
+}
+
 // define the path the the templates
 define('CRAFT_TEMPLATES_PATH', '../resources/templates/');
 
