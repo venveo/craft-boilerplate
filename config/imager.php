456cbc1ac7682ad83041bcd566922b98524906ec
Ransom <ransom@venveo.com>
2018-06-10 21:33:03 -0400

    Added imager config

---
+++ config/imager.php
@@ -0,0 +1,22 @@
+<?php
+
+// We're gonna use S3 for our image transforms.
+return [
+    'imagerUrl' => getenv('ASSETS_BASE_URL') . '/transforms/',
+    'removeMetadata' => true,
+    'storages' => ['aws'],
+    'transformer' => 'craft',
+    'storageConfig' => [
+        'aws' => [
+            'accessKey' => getenv('AWS_KEY_ID'),
+            'secretAccessKey' => getenv('AWS_SECRET'),
+            'region' => getenv('AWS_REGION'),
+            'bucket' => getenv('AWS_S3_BUCKET'),
+            'folder' => 'transforms',
+            'requestHeaders' => array(),
+            'storageType' => 'standard',
+            'cloudfrontInvalidateEnabled' => true,
+            'cloudfrontDistributionId' => getenv('AWS_CF_ID'),
+        ]
+    ]
+];
