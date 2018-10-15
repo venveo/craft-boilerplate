29995b2e1401723b66acee25fa9a5dd0467f0c5b
nikkijd <nj@venveo.com>
2018-10-15 12:34:12 +0800

    Update some JS structure and SCSS cleanup

--- frontend/resources/assets/js/components/index.js
+++ frontend/resources/assets/js/components/index.js
@@ -1,3 +1,4 @@
 // Initial JavaScript Component Loader
 // 1. If a component file becomes longer than 50 lines, break it into another component.
-export { default as siteUI } from './ui'
+export { default as globalJS } from './global'
+export { default as pagesJS } from './pages'
